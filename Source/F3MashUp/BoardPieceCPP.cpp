6ddf0a87126a46d64ebd814b764d6a708cb301c0
chung <colinhung03@gmail.com>
2020-03-01 18:58:04 -0500

    Adding CPP files meant to be base classes and enable CPP and BP hybrid objects

---
+++ Source/F3MashUp/BoardPieceCPP.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BoardPieceCPP.h"
+
+// Sets default values
+ABoardPieceCPP::ABoardPieceCPP()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABoardPieceCPP::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABoardPieceCPP::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
