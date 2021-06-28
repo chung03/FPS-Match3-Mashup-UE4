dd549f53592d14f48841d08f09bd2b3a1d4b83f2
chung <colinhung03@gmail.com>
2021-06-28 13:02:57 -0400

    Fixing the map to put players in different locations at spawn

---
+++ Source/F3MashUp/ScorePickUpCPP.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ScorePickUpCPP.generated.h"
+
+UCLASS()
+class F3MASHUP_API AScorePickUpCPP : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AScorePickUpCPP();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
